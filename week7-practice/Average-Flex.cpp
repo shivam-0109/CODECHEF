110a212a2aede8fcae99405c850340da12abbbb9
Shivam kumar singh <sm1788@srmist.edu.in>
2022-10-16 21:32:54 +0530

    Create Average-Flex.cpp

---
+++ week7-practice/Average-Flex.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+	int t;
+	cin>>t;
+	while(t--){
+	  int n;
+      cin>>n;
+      int arr[n];
+      for (int i = 0; i < n; i++)
+      {
+          cin>>arr[i];
+      }
+      sort(arr,arr+n);
+      int mid = (n) / 2;
+      int total = n-mid;
+
+        for (int i = 0; i < mid;i++)
+        {
+            if(arr[i]==arr[mid])
+                total++;
+        }
+
+            cout <<total<< endl;
+      
+	}
+	return 0;
+}
